eb358f3726cf6317f6d20ccde8bd72f62d76e70a
Christopher Pepi <cpepi@draper.com>
2018-08-10 12:52:20 -0400

    multiple ball support

---
+++ launcher.py
@@ -0,0 +1,35 @@
+from graphics import *
+from math import sin, cos, radians, degrees
+from shottracker import ShotTracker
+
+class Launcher:
+
+    def adjAngle(self, amt):
+        self.angle = self.angle + radians(amt)
+        self.redraw()
+
+    def adjVel(self, amt):
+        self.vel = self.vel + amt
+        self.redraw()
+
+    def redraw(self):
+        self.arrow.undraw()
+        pt2 = Point(self.vel*cos(self.angle), self.vel*sin(self.angle))
+        self.arrow = Line(Point(0, 0), pt2).draw(self.win)
+        self.arrow.setArrow("last")
+        self.arrow.setWidth(3)
+
+    def fire(self):
+        return ShotTracker(self.win, degrees(self.angle), self.vel, 0.0)
+
+    def __init__(self, win):
+        base = Circle(Point(0, 0), 3)
+        base.setFill("green")
+        base.setOutline("red")
+        base.draw(win)
+
+        self.win = win
+        self.angle = radians(45.0)
+        self.vel = 40.0
+        self.arrow = Line(Point(0, 0), Point(0, 0)).draw(win)
+        self.redraw()
